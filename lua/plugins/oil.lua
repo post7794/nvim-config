a8efe304d2356957bfc3dd0c38deb0c81f690344
post7794 <post7794@users.noreply.github.com>
2026-04-24 06:17:30 +0800

    Enhance oil sidebar: toggle focus, q to close, CR to open file in editor

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -15,13 +15,45 @@ return {
             },
             keymaps = {
                 ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+                ["q"] = function()
+                    vim.cmd.close()
+                end,
+                ["<CR>"] = function()
+                    local oil = require("oil")
+                    local entry = oil.get_cursor_entry()
+                    if not entry then
+                        return
+                    end
+                    if entry.type == "directory" then
+                        oil.select()
+                        return
+                    end
+                    local dir = oil.get_current_dir()
+                    if not dir then
+                        return
+                    end
+                    vim.cmd.wincmd("p")
+                    vim.cmd.edit(vim.fn.fnameescape(dir .. entry.name))
+                end,
             },
         })
 
+        local oil_buf = nil
+
         vim.keymap.set("n", "<leader>e", function()
+            if oil_buf and vim.api.nvim_buf_is_valid(oil_buf) and #vim.fn.win_findbuf(oil_buf) > 0 then
+                local oil_win = vim.fn.win_findbuf(oil_buf)[1]
+                if vim.api.nvim_get_current_win() == oil_win then
+                    vim.cmd.wincmd("p")
+                else
+                    vim.fn.win_gotoid(oil_win)
+                end
+                return
+            end
             vim.cmd("leftabove vsplit")
             vim.cmd("vertical resize 25")
             require("oil").open()
+            oil_buf = vim.api.nvim_get_current_buf()
         end, { desc = "Open Oil File Manager", silent = true })
     end,
 }
