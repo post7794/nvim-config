dc73b3659785cf5ee418c2bbac5e3364bd3c5c28
post7794 <7794@tutamail.com>
2026-04-25 20:04:45 +0800

    fix: 修正 Space+rc 调用 crateGraph 而非 openCargo

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -23,7 +23,7 @@ return {
 				vim.keymap.set("n", "<leader>re", function() vim.cmd.RustLsp("expandMacro") end, vim.tbl_extend("force", opts, { desc = "Rust Expand Macro" }))
 				vim.keymap.set("n", "<leader>ro", function() vim.cmd.RustLsp("openCargo") end, vim.tbl_extend("force", opts, { desc = "Rust Open Cargo.toml" }))
 				vim.keymap.set("n", "<leader>rp", function() vim.cmd.RustLsp("parentModule") end, vim.tbl_extend("force", opts, { desc = "Rust Parent Module" }))
-				vim.keymap.set("n", "<leader>rc", function() vim.cmd.RustLsp("openCargo") end, vim.tbl_extend("force", opts, { desc = "Rust Crate Graph" }))
+				vim.keymap.set("n", "<leader>rc", function() vim.cmd.RustLsp("crateGraph") end, vim.tbl_extend("force", opts, { desc = "Rust Crate Graph" }))
 				vim.keymap.set("n", "<leader>rs", function() vim.cmd.RustLsp("ssr") end, vim.tbl_extend("force", opts, { desc = "Rust SSR" }))
 				vim.keymap.set("n", "<leader>rh", function() vim.cmd.RustLsp({ "hover", "actions" }) end, vim.tbl_extend("force", opts, { desc = "Rust Hover Actions" }))
 			end,
